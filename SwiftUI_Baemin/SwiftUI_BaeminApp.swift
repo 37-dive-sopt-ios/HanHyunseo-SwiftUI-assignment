d71fc8ac99451325641c3ac1109c55bc7dbd274c
hyunseo-han <ols3040015@naver.com>
2025-12-09 20:25:03 +0900

    Initial Commit

---
+++ SwiftUI_Baemin/SwiftUI_BaeminApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BaeminApp.swift
+//  SwiftUI_Baemin
+//
+//  Created by Hyunseo Han on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BaeminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
